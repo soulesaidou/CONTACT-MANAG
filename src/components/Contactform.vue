62b4148e553abf61860783b32ecca341488c0dbd
soulesaidou <saidou.soule11@laposte.net>
2019-07-28 16:20:57 +0200

    feat: initialisation du projet

---
+++ src/components/Contactform.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <form action="">
+            <input type ="text"> placeholder = "prénom">
+            <input type ="text"> placeholder = "nom">
+            <input type ="text"> placeholder = "email">
+            <input type ="text"> placeholder = "téléphone">
+            <button type="submit">créer</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+              firstName: "",
+              lastName: "",
+              email: "",
+              phone: ""
+            };
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
