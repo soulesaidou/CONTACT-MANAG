1f0781f56faefb010eda5fc7aa68eca1f0116178
soulesaidou <saidou.soule11@laposte.net>
2019-07-29 16:56:15 +0200

    feat: sans erreur modifier par yanis

---
+++ src/config.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line linebreak-style
+/* eslint-disable eol-last */
+const firebaseConfig = {
+    apiKey: 'AIzaSyBbzW9A8pjI8WE6_MEzxSnBuvGZ9grf8hY',
+    authDomain: 'contact-manag.firebaseapp.com',
+    databaseURL: 'https://contact-manag.firebaseio.com',
+    projectId: 'contact-manag',
+    storageBucket: 'contact-manag.appspot.com',
+    messagingSenderId: '196737996223',
+    appId: '1:196737996223:web:23ac1742c73416e4',
+};
+
+export default firebaseConfig;
