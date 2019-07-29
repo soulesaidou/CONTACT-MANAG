900bcad5d22dc4e1da6c5c6b0e0d02c2823f9e83
soulesaidou <saidou.soule11@laposte.net>
2019-07-29 14:57:15 +0200

    feat: correction de toute les erreurs

--- src/components/Contactform.vue
+++ src/components/Contactform.vue
@@ -1,24 +1,48 @@
+
 <template>
     <div>
-        <form action="">
-            <input type ="text"> placeholder = "prénom">
-            <input type ="text"> placeholder = "nom">
-            <input type ="text"> placeholder = "email">
-            <input type ="text"> placeholder = "téléphone">
+        <h2>Créer un contact</h2>
+        <form @submit.prevent="createContact">
+            <!-- ".prevent" évite le comportement par defaut du formulaire -->
+            <!-- v-on ou @ permettes (d'écouter une élément) de faire la soumission des méthode
+            créer et écouté l'évenement du formulaire , dans ce cas la c'est submit -->
+            <!--v-model permet de faire le lien entre le script et les template-->
+            <input type ="text" placeholder = "prénom" v-model="contact.firstName"><br>
+            <input type ="text" placeholder = "nom" v-model="contact.lastName"><br>
+            <input type ="text" placeholder = "email" v-model="contact.email"><br>
+            <input type ="text" placeholder = "téléphone" v-model="contact.phone"><br>
             <button type="submit">créer</button>
         </form>
     </div>
 </template>
 
 <script>
-    export default {
-        data(){
-            return{
-              firstName: "",
-              lastName: "",
-              email: "",
-              phone: ""
-            }; 
-        }
-   }
+// import { METHODS } from 'http';
+export default {
+  data() {
+    return {
+      contact: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+      },
+    };
+  },
+  // methode va nous permettre de créer la soumission de notre formulaire de contact
+  methods: {
+    createContact() {
+      console.log('this.contact');
+      // pour accéder au élement du contact ont utilise l'élement this
+    },
+  },
+};
+// vdata permet de generer une structure vierge d'une fonction data
 </script><style lang="scss" scoped></style>
+
+<style scoped> /* le scoped permet de pouvoir modifier uniquement sur le composent choisie*/
+h2
+{
+    color: red;
+}
+</style>
